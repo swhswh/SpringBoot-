6c8ba86641e2ab100cc98345c6d14c1710f5a636
swhswh <30549469+swhswh@users.noreply.github.com>
2020-01-11 22:48:18 +0800

    Create SQLFilter.java

---
+++ src/main/java/com/doubles/crea_server/core/common/xss/SQLFilter.java
@@ -0,0 +1,44 @@
+package com.doubles.crea_server.core.common.xss;
+
+import com.doubles.crea_server.core.common.enums.ErrorEnum;
+import com.doubles.crea_server.core.common.exception.MyException;
+import org.springframework.util.StringUtils;
+
+/**
+ * SQLFilter
+ *
+ * @author swh
+ * @date 2020-01-11
+ * @description: SQL过滤
+ */
+public class SQLFilter {
+    /**
+     * SQL注入过滤
+     * @param str  待验证的字符串
+     */
+    public static String sqlInject(String str){
+        if(StringUtils.isEmpty(str)){
+            return null;
+        }
+        //去掉'|"|;|\字符
+        str = StringUtils.replace(str, "'", "");
+        str = StringUtils.replace(str, "\"", "");
+        str = StringUtils.replace(str, ";", "");
+        str = StringUtils.replace(str, "\\", "");
+
+        //转换成小写
+        str = str.toLowerCase();
+
+        //非法字符
+        String[] keywords = {"master", "truncate", "insert", "select", "delete", "update", "declare", "alert", "drop"};
+
+        //判断是否包含非法字符
+        for(String keyword : keywords){
+            if(str.indexOf(keyword) != -1){
+                throw new MyException(ErrorEnum.SQL_ILLEGAL);
+            }
+        }
+
+        return str;
+    }
+}
