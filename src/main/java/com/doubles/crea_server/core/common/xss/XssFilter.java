3af5a9dba591be3149b1f75a85100a86910f2045
swhswh <30549469+swhswh@users.noreply.github.com>
2020-01-11 22:49:08 +0800

    Add files via upload

---
+++ src/main/java/com/doubles/crea_server/core/common/xss/XssFilter.java
@@ -0,0 +1,30 @@
+package com.doubles.crea_server.core.common.xss;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+/**
+ * XssFilter
+ *
+ * @author swh
+ * @date 2020-01-11
+ * @description XssFilter过滤
+ */
+public class XssFilter implements Filter{
+    @Override
+    public void init(FilterConfig config) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        XssHttpServletRequestWrapper xssRequest = new XssHttpServletRequestWrapper(
+                (HttpServletRequest) request);
+        chain.doFilter(xssRequest, response);
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
